09e0469ecaba5da99147f82e593c92f56bd59859
Matt Guttman <msguttman@gmail.com>
2020-04-17 18:15:26 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,4 +4,8 @@
 
 def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
-  puts "A plate of #{"
+  order = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts order
+  order
+end
+  
